389f3b0aa3d16357f4b2dc2d472f996063b10f88
Selcukibar <selcukibarr@icloud.com>
2024-04-18 14:27:30 +0300

    gender

--- eccomerce/products/models.py
+++ eccomerce/products/models.py
@@ -4,6 +4,7 @@ from django.utils.text import slugify
 
 # Create your models here.
 
+
 class Category(models.Model):
     name = models.CharField("Kategori", max_length=100)
     
@@ -11,14 +12,19 @@ class Category(models.Model):
         return self.name
 
 class Product(models.Model):
+    genders = (
+        ('M','Erkek'),
+        ('F','Kadın'),
+    )
     title = models.CharField("Başlık", max_length=150)
     description = models.TextField("Açıklama")
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     old_price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     image = models.ImageField(upload_to="products")
     slug = models.SlugField(unique=True, db_index=True, default="")
-    is_active = models.BooleanField()
-    is_home = models.BooleanField()
+    is_active = models.BooleanField(default=True)
+    is_home = models.BooleanField(default=True)
+    gender = models.CharField("Cinsiyet", max_length=1, choices=genders, default=1)
     category = models.ManyToManyField(Category)
 
     class Meta:
@@ -27,7 +33,7 @@ class Product(models.Model):
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title)
-        super().save(args, kwargs)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.title} {self.price}"
